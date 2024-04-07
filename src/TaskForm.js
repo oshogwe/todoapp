c79e43e913130a768f041195771a303dd7641880
oshogwe ikekhai <osho/ikekhai@yahoo.com>
2024-04-07 01:36:46 +0100

    first commit

---
+++ src/TaskForm.js
@@ -0,0 +1,18 @@
+import {useState} from 'react';
+
+export default function TaskForm({onAdd}){
+    const [taskName, setTaskName] = useState('');
+    function handleSubmit(ev){
+      ev.preventDefault();
+      onAdd(taskName);
+      setTaskName('');
+    }
+    return(
+        <form onSubmit={handleSubmit}>
+        <button>+</button>
+        <input type='text' value={taskName} 
+        onChange={ev=> setTaskName(ev.target.value)}
+        placeholder="your next task..."/>
+      </form>  
+    );
+}
